f89378209396a12ae09baff6ee564887decef40f
Jake Rhoads <jake.rhoads@gmail.com>
2020-06-19 15:39:00 -0400

    Mongo Connection with Heroku

--- server.js
+++ server.js
@@ -10,10 +10,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://Workout-Tracker:R$CV3WJXmxsC9JR@ds061371.mlab.com:61371/heroku_2wz0h0c7", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
+
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+// mongoose.connect(MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// })
+
 
 // routes
 app.use(require("./routes/api.js"));
